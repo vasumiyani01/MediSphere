ed4338fa1e376a841337ec677dbd7db7477583ee
mayank122340 <118726179+mayank122340@users.noreply.github.com>
2026-07-18 00:21:02 +0530

    Add dynamic OTP signup and login system with CustomUser database schema

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
 
-# Register your models here.
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    list_display = ['username', 'mobile_number', 'email', 'is_staff', 'is_active']
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {'fields': ('mobile_number',)}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (None, {'fields': ('mobile_number',)}),
+    )
+
+admin.site.register(CustomUser, CustomUserAdmin)
+
