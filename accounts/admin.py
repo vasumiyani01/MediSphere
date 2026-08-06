d4bef81a877ded82158c9346797151cd743ee871
Vasu Miyani <vasumiyani01@gmail.com>
2026-08-06 12:14:27 +0530

    Delete accounts directory

--- accounts/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import UserProfile
-
-class UserProfileAdmin(admin.ModelAdmin):
-    list_display = ['email', 'name', 'mobile_number', 'user_type', 'date_joined']
-    search_fields = ['name', 'mobile_number', 'email']
-    list_filter = ['user_type']
-
-admin.site.register(UserProfile, UserProfileAdmin)
