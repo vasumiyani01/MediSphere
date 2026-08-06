b4dc69f2cb0d996ed0178f5780fc657373051b94
Vasu Miyani <vasumiyani01@gmail.com>
2026-08-06 12:14:48 +0530

    Delete doctors directory

--- doctors/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.doctor_dashboard, name='doctor_dashboard'),
-    path('update-doctor-profile/', views.update_doctor_profile, name='update_doctor_profile'),
-]
