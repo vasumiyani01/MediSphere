8e42a7ca3afb9de489429001dc1209df0322f507
Vasu Miyani <vasumiyani01@gmail.com>
2026-08-06 12:14:54 +0530

    Delete diseases directory

--- diseases/admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib import admin
-from .models import Disease
-
-admin.site.register(Disease)
--- diseases/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class DiseasesConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'diseases'
